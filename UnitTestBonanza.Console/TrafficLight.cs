0b18db7d20d8244944f6cad69511b5748fb8c6ac
GET Academy <terje@getacademy.no>
2022-05-24 11:06:28 +0200

    x

--- UnitTestBonanza.Console/TrafficLight.cs
+++ UnitTestBonanza.Console/TrafficLight.cs
@@ -5,13 +5,13 @@
         private int _phase = 0;
         public bool IsRed => _phase < 2;
         public bool IsYellow => _phase % 2 == 1;
-        public bool IsGreen => _phase==2;
+        public bool IsGreen => _phase == 2;
 
         public void Next()
         {
-            var b1 = _phase == 1 || _phase == 2;
-            var b2 = _phase is 1 or 2;
-
+            // ternary operator
+            //_phase = _phase < 3 ? _phase + 1 : 0;
+            _phase = (_phase + 1) % 4;
         }
     }
 }
--- UnitTestBonanza.Test/TrafficLightTest.cs
+++ UnitTestBonanza.Test/TrafficLightTest.cs
@@ -5,18 +5,78 @@ namespace UnitTestBonanza.Test
 {
     /*
       - teknisk sett, hvordan lage unit test
-      - testdrevet utvikling
+      - testdrevet utvikling - TDD 
       - kodedekning
      */
     public class TrafficLightTest
     {
         [Test]
-        public void TestStartsWithRed()
+        public void TestPhase1()
         {
             var trafficLight = new TrafficLight();
             Assert.IsTrue(trafficLight.IsRed);
             Assert.IsFalse(trafficLight.IsYellow);
             Assert.IsFalse(trafficLight.IsGreen);
+        }
+
+        [Test]
+        public void TestPhase2()
+        {
+            var trafficLight = new TrafficLight();
+            trafficLight.Next();
+            Assert.IsTrue(trafficLight.IsRed);
+            Assert.IsTrue(trafficLight.IsYellow);
+            Assert.IsFalse(trafficLight.IsGreen);
+        }
+
+        [Test]
+        public void TestPhase3()
+        {
+            var trafficLight = new TrafficLight();
+            trafficLight.Next();
+            trafficLight.Next();
+            Assert.IsFalse(trafficLight.IsRed);
+            Assert.IsFalse(trafficLight.IsYellow);
+            Assert.IsTrue(trafficLight.IsGreen);
+        }
+
+        [Test]
+        public void TestPhase4()
+        {
+            var trafficLight = new TrafficLight();
+            trafficLight.Next();
+            trafficLight.Next();
+            trafficLight.Next();
+            Assert.IsFalse(trafficLight.IsRed);
+            Assert.IsTrue(trafficLight.IsYellow);
+            Assert.IsFalse(trafficLight.IsGreen);
+        }
+
+        [Test]
+        public void TestPhase1AfterPhase4()
+        {
+            // Arrange
+            var trafficLight = new TrafficLight();
+            trafficLight.Next(); // fase 2
+            trafficLight.Next(); // fase 3
+            trafficLight.Next(); // fase 4
+
+            // Act
+            trafficLight.Next(); // fase 1
+
+            // Assert
+            Assert.IsTrue(trafficLight.IsRed);
+            Assert.IsFalse(trafficLight.IsYellow);
+            Assert.IsFalse(trafficLight.IsGreen);
+        }
+
+        private static void DoNexts(int count, TrafficLight t)
+        {
+            while (count > 0)
+            {
+                t.Next();
+                count--;
+            }
         }
     }
 }
